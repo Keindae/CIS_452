18351f6be8d5cc2c555c23d4bbe9df1462032cdf
Matt Noblett <mattnoblett@Matts-MacBook-Pro.local>
2017-04-05 16:10:39 -0400

    Working Project 3

---
+++ Project3/Mole.java
@@ -0,0 +1,29 @@
+/*
+Matt Noblett
+Whack-a-Mole
+Wolffe - GVSU 2017
+Project 3
+*/
+import javax.swing.*;
+
+public class Mole extends JFrame{
+
+
+  public Mole(){
+      // JFrame frame = new JFRame();
+      JPanel panel = new JPanel();
+      this.setLayout(new BorderLayout());
+      this.add(panel, BorderLayout.CENTER);
+
+      // panel.setContraints(new something());
+
+  }
+
+
+
+  public static void main(String args[]){
+    Mole mole = new Mole();
+  }
+
+
+}
